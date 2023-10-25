d5b8ae63f5756084ece3c05caebc319b44daa15d
Pranita Pendharkar <pendharkarpranita@gmail.com>
2023-10-25 10:53:08 +0200

    added pagination and search icon

--- MealPlanner_Frontend/src/Components/SearchRecipe.jsx
+++ MealPlanner_Frontend/src/Components/SearchRecipe.jsx
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import "../Styles/SearchRecipe.css";
 
 function SearchRecipe() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-
   const appId = import.meta.env.VITE_API_ID;
   const appKey = import.meta.env.VITE_APP_KEY;
 
-  //fetch function
-
-  useEffect(() => {
+  const handleSearch = () => {
     if (query) {
       const edamamAPIUrl = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&from=0&to=6`;
 
@@ -28,16 +24,10 @@ function SearchRecipe() {
       // Clear recipes if the query is empty
       setRecipes([]);
     }
-  }, [query]);
+  };
+
   return (
     <div>
-      {/* <input
-      type="text"
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      placeholder="Search for recipes"
-    /> */}
-
       <div className="wrap">
         <div className="search">
           <input
@@ -47,34 +37,29 @@ function SearchRecipe() {
             onChange={(e) => setQuery(e.target.value)}
             placeholder="What are you looking for?"
           />
+          <button className="searchButton" onClick={handleSearch}>
+          <i className="fa-solid fa-magnifying-glass fa-spin" style={{color: "#f0f2f5"}}></i> {/* Add a search icon here */}
+          </button>
         </div>
       </div>
       <ul>
         {recipes.map((recipe) => (
-          <>
-            <div className="gallery">
-              <a target="_blank" href="img_5terre.jpg">
-                <img
-                  src={recipe.recipe.image}
-                  alt={recipe.recipe.label}
-                  width="600"
-                  height="300px"
-                />
+          <div className="gallery" key={recipe.recipe.label}>
+            <a target="_blank" href="img_5terre.jpg">
+              <img
+                src={recipe.recipe.image}
+                alt={recipe.recipe.label}
+                width="600"
+                height="300px"
+              />
+            </a>
+            <p className="card-title">{recipe.recipe.label}</p>
+            <button className="card-btn">
+              <a href={recipe.recipe.url} target="_blank" rel="noopener noreferrer">
+                <h4>View Recipe</h4>
               </a>
-              <p className="card-title">{recipe.recipe.label}</p>
-              <button className="card-btn">
-                {" "}
-                <a
-                  href={recipe.recipe.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {/* View Recipe Instructions */}
-                  <h4>View Recipe</h4>
-                </a>
-              </button>
-            </div>
-          </>
+            </button>
+          </div>
         ))}
       </ul>
     </div>
