be71d08119bebfbd98bbb188e1c2b9e65909c450
Pranita Pendharkar <pendharkarpranita@gmail.com>
2023-11-06 13:21:15 +0100

    Added grocery list and search bar functionality in all recipe page

--- MealPlanner_Frontend/src/Components/Favorite.jsx
+++ MealPlanner_Frontend/src/Components/Favorite.jsx
@@ -1,22 +1,9 @@
-
-
 import { Card } from 'antd';
 import React, { useEffect, useState } from 'react';
 import "../Styles/AllRecipes.css";
 import { FaFacebook, FaTwitter, FaPinterest } from 'react-icons/fa';
 
-const { Meta } = Card;
-  // Function to share a recipe on Facebook
-  const shareOnFacebook = (recipe) => {
-    const url = `https://www.facebook.com/sharer.php?u=${recipe.url}`;
-    window.open(url, '_blank');
-  };
-
-  // Function to share a recipe on Twitter
-  const shareOnTwitter = (recipe) => {
-    const url = `https://twitter.com/intent/tweet?text=${recipe.label}&url=${recipe.url}`;
-    window.open(url, '_blank');
-  };
+const { Meta } = Card
 
  
 const Favorite = () => {
@@ -28,20 +15,7 @@ const Favorite = () => {
       setFavorites(JSON.parse(storedFavorites));
     }
   }, []);
-    // Function to share a recipe
-    const shareRecipe = (recipe) => {
-      if (navigator.share) {
-        navigator.share({
-          title: recipe.label,
-          text: 'Check out this delicious recipe!',
-          url: recipe.url,
-        })
-          .then(() => console.log('Recipe shared successfully.'))
-          .catch((error) => console.error('Error sharing recipe:', error));
-      } else {
-        alert('Sharing is not supported on this browser.');
-      }
-    };
+
   return (
     <div className="recipes-container">
       <h3>Your Favorite Recipes</h3>
@@ -65,17 +39,6 @@ const Favorite = () => {
       </div>
               </a>
             </Card>
-{/*             
-           
-            <div className="share-buttons">
-            <button className="btn-add-planner" onClick={() => shareRecipe(recipe)}>Share</button>
-            
-                <i className="fa-brands fa-facebook " style={{color: "#1877F2"}} onClick={() => shareOnFacebook(recipe)}></i>
-            
-                <i className="fa-brands fa-twitter " style={{color: "#feda75"}} onClick={() => shareOnTwitter(recipe)}></i>
-
-             
-                </div> */}
           </div>
         ))}
       </div>
